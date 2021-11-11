9d31ed1d445dec7797a636aa07b77439c23498a2
Potanz <ahsie014@ucr.edu>
2021-11-10 20:27:49 -0800

    moved all the files into src folder

--- src/validity.cpp
+++ src/validity.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void checkCoord(int coord, string val) {
+void checkCoord(int& coord, string val) {
     while(coord < 1 || coord > 10) {
             cout << "That is not a valid coordinate. Try again!" << endl;
             if(val == "x") {
