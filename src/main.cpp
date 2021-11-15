c1d36ae41299c9f3b49e08bafa4c56c1fe84fca5
Potanz <ahsie014@ucr.edu>
2021-11-14 18:01:48 -0800

    changed main slightly, replaced how we responded to user's sship size input

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
+#include <vector>
 #include <sstream>
-#include "board.h"
-#include "validity.h"
-#include "ships.h"
+#include "board.cpp"
+#include "validity.cpp"
 using namespace std;
 
 int main() {
+    vector<int> shipSizes = { 5, 4, 3, 3, 2 };
     string matrix[row][column];
     string userName = "";
+    int sizeInput = 0;
     char confirmation = '\0';
-    string shipSize = "";
     int shipValue = 0;
-    int xCoord = 0;
-    int yCoord = 0;
-    int attackCoordx = 0; 
-    int attackCoordy = 0; 
+    int x = 0;
+    int y = 0; 
 
     clearMatrix(matrix);
     grid(matrix);
@@ -22,33 +21,45 @@ int main() {
     cout << "What is your game name?" << endl;
     cin >> userName;
 
-    cout << "Which ship size would you like to pick first?" << endl;
-    cin >> shipSize; // maybe this should be an array so we can erase a value?
-    stringstream convert(shipSize);
-    convert >> shipValue;
-
-    while(!(shipValue > 1 && shipValue < 6)) { // we can for loop to iterate an array to check deleted value
-        cout << "That is not a valid ship size. Try again!" << endl;
-        cin >> shipValue;
-        stringstream convert(shipValue);
-        convert >> shipSize;
-    }
+    cout << "Which ship size would you like to place first?" << endl;
+    cin >> sizeInput;
+    for(int i = 0; i < shipSizes.size(); i++) {
+        if(shipSizes.at(i) == sizeInput) {
+            if(shipSizes.at(i) == 5) {
+                //fiveShip();
+            }
+            else if (shipSizes.at(i) == 4) {
+                //fourShip();
+            }
+            else if (shipSizes.at(i) == 3) {
+                //threeShip();
+            }
+            else if (shipSizes.at(i) == 2) {
+                //twoShip();
+            }
+            shipSizes.erase(shipSizes.begin() + i);
+        }
+        else {
+            cout << "That is not a valid ship size. Try again!" << endl;
+            cin >> sizeInput;
+        }
+    } // maybe this should be an array so we can erase a value?
     cout << "What coordinates do you want to start your ship at? (type the X and Y value of the first position)" << endl;
     // we will have to factor the coordinates with respect to the grid
     cout << "x: ";
-    cin >> xCoord;
-    checkCoord(xCoord, "x");
+    cin >> x;
+    checkCoord(x, "x");
     cout << "y: ";
-    cin >> yCoord;
-    checkCoord(yCoord, "y");
+    cin >> y;
+    checkCoord(y, "y");
     cout << "Which direction do you want to ship to end at? (type the X and Y value of the last position)" << endl;
     // need first test case here based on ship size to see if end location is valid
     cout << "x: ";
-    cin >> xCoord;
-    checkCoord(xCoord, "x");
+    cin >> x;
+    checkCoord(x, "x");
     cout << "y: ";
-    cin >> yCoord;
-    checkCoord(yCoord, "y");
+    cin >> y;
+    checkCoord(y, "y");
 
     cout << "Are you sure that is your final board placement? (y/n)" << endl;
     cin >> confirmation;
@@ -59,8 +70,8 @@ int main() {
 
 
     cout << "Which coordinate would you like to attack?" << endl;
-    cin >> attackCoordx;
-    cin >> attackCoordy;
+    cin >> x;
+    cin >> y;
 
     return 0;
 }
