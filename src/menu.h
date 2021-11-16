fe520cd3dddaaa52a506b8b4e6fee51f0d5cfc94
angelikab028 <angelikabermudez477@gmail.com>
2021-11-15 17:47:14 -0800

    Adding Menu class header file

---
+++ src/menu.h
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class Menu {
+	private:
+		string userInput;
+	public:
+		void start_game();
+		void easy();
+		void hard();
+		void speed();
+		void powerups_on_or_off();
+		void load_game();
+
+};
